d83a317a74b706785a2d238154ed196989496a63
zacharychoughton <zacharyh2001@gmail.com>
2023-03-14 18:59:38 -0400

    --

--- threads.c
+++ threads.c
@@ -155,7 +155,7 @@ void pthread_create_helper(){
     timing.it_value = timeval;
     const struct itimerval* timeptr = &timing; 
 
-    s = setitimer(ITIMER_REAL,timeptr,NULL); //sets up regular SIGALRM intervals. 
+    s = setitimer(ITIMER_VIRTUAL,timeptr,NULL); //sets up regular SIGALRM intervals. 
     if (s == -1){ 
         printf("Error creating timer\n");
         exit(1); 
@@ -165,7 +165,7 @@ void pthread_create_helper(){
     sigemptyset(&sighandler.sa_mask);
     sighandler.sa_handler = &schedule; 
     sighandler.sa_flags = SA_NODEFER; 
-    sigaction(SIGALRM, &sighandler, NULL); 
+    sigaction(SIGVTALRM, &sighandler, NULL); 
 } 
 
 /**********************schedule************************/
