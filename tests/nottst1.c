26a9ab4679faaa1c5b9c6b29d0f65ddc350a44ad
zacharychoughton <zacharyh2001@gmail.com>
2023-03-14 20:45:02 -0400

    sdfa

--- tests/nottst1.c
+++ tests/nottst1.c
@@ -23,7 +23,7 @@ int main(){
     const pthread_attr_t *attr; //attricbute about thread, always null for this. 
     void *args = 0; 
     attr = NULL; 
-    void *(*passfunc_ptr)(void*) = &passfunc; 
+    void *(*passfunc_ptr)(void*) = passfunc; 
     int i; 
 
     for( int i = 0; i<5; i++){
