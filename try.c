96ff9f6390300e40048422b66a035310b0c63131
zacharychoughton <zacharyh2001@gmail.com>
2023-03-14 23:13:43 -0400

    fsd

--- try.c
+++ try.c
@@ -19,15 +19,15 @@ int main() {
   pthread_t tid[20];
   int j; 
 
-  for (i=0;i<20;i++){
+  for (j=0;j<20;j++){
 
-    int ret = pthread_create(&tid[i], NULL, thread_start, NULL);
+    int ret = pthread_create(&tid[j], NULL, thread_start, NULL);
     if (ret != 0) {
     fprintf(stderr, "Error creating thread\n");
     exit(EXIT_FAILURE);
     }
-  printf("Thread IDs: %d %d\n",tid[i],i);
-  i++; 
+  printf("Thread IDs: %d %d\n",tid[j],j);
+  j++; 
   }
   printf("Main Thread ID %d\n",pthread_self());
   pthread_exit(NULL);
